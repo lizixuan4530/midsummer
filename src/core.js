ec48c7d4d19c18e4e8cbc37dddd2c3154647eb46
lizixuan <lizixuan4530@gmail.com>
2021-02-05 21:35:56 +0100

    add newyear

--- src/core.js
+++ src/core.js
@@ -294,7 +294,7 @@ const Newyear = [
     imgs:[Bao_1,Bao_2,Bao_3,Bao_4],
     title: '黑金流沙包',
     price1: '1个...........5€',
-    price2: '预定冷冻流沙包，回家蒸一下哦',
+    price2: '冷冻流沙包，回家蒸一下哦',
   },]
 
 const Customized_p = [Customized_p_1,Customized_p_2,Customized_p_3,Customized_p_4,
